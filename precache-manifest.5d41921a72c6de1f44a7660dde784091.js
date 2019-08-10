bd96bfd0524dec97b47b4a221233b358ce99bf51
Nurzhan <nurzhann173@gmail.com>
2019-08-10 15:21:05 +0600

    Updates

---
+++ precache-manifest.5d41921a72c6de1f44a7660dde784091.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f73058161e71fa51d160e8c27c93be80",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "599bd8be4f82ec928398",
+    "url": "/dashboard/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "64d7d964ca61710aa3a2",
+    "url": "/dashboard/static/js/2.fbdfa904.chunk.js"
+  },
+  {
+    "revision": "599bd8be4f82ec928398",
+    "url": "/dashboard/static/js/main.10bc6755.chunk.js"
+  },
+  {
+    "revision": "5f13b0ac945b76fd7e5e",
+    "url": "/dashboard/static/js/runtime~main.d3aff377.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
