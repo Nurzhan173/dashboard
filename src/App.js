fd38e22b50d6d4bc0fce9e49af496abda6ae9ed5
Nurzhan <nurzhann173@gmail.com>
2019-08-10 18:58:12 +0600

    add charts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Chart from './components/Chart';
+import Header from './components/header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Nurzhan<code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      chartData:{}
+    }
+  }
+
+  componentWillMount(){
+    this.getChartData();
+  }
+
+  getChartData(){
+    // Ajax calls here
+    this.setState({
+      chartData:{
+        labels: ['Август', 'Январь', 'Март', 'Апрель', 'Сентябрь', 'Октябрь'],
+        datasets:[
+          {
+            label:'Расходы',
+            data:[
+              617594,
+              181045,
+              153060,
+              106519,
+              105162,
+              95072
+            ],
+            backgroundColor:[
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)'
+            ]
+          }
+        ]
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Chart chartData={this.state.chartData} location="Almaty" legendPosition="bottom"/>
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
+
+class Header extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>DASHBOARD</h1>
+      </div>
+    )
+  }
+}
+
+export default Header;
